3e5129b4d6e455b59ca0f7c95f29366b564fe08c
Chris <chrisbiemer@ymail.com>
2022-11-04 10:49:22 -0500

    Update pandasBoiler.py

--- pandasBoiler.py
+++ pandasBoiler.py
@@ -1,6 +1,43 @@
+#pip install numpy
 #pip install pandas
-#pip install openpyxl
+#pip install sklearn
+#pip install matplotlib
+import numpy as np
 import pandas as pd
-df = pd.io.excel.read_excel(r'comple/file/path/inputData.xlsx');
-print(df)
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+#importing csv file
+df = pd.read_csv("cleaned.csv");
+
+df2 = pd.DataFrame(df)
+
+#setting independent and dependent variables
+X = df[['income', 'metro_status', 'education']]
+y = df['numvisits']
+
+#perform a covariate analysis 
+X, y = np.array(X), np.array(y)
+#add a constant to the independent variables
+X = sm.add_constant(X)
+model = sm.OLS(y, X)
+results = model.fit()
+print(results.summary())
+
+#scatter chart using pyplot
+#plt.scatter(df['education'], df['numvisits'], color='red')
+#plt.title('chart', fontsize=14)
+#plt.xlabel('education', fontsize=14)
+#plt.ylabel('numvisits', fontsize=14)
+#plt.grid(True)
+#plt.show()
+
+#predictive output from a given input
+#regr = linear_model.LinearRegression()
+#regr.fit(X, y)
+#predictedV = regr.predict([[16, 2, 45, 40]])
+#print(predictedV)
+
+exit()
 
